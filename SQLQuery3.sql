dfc83f85e772cf4def0d898fd3e1925c6f4c209b
Quang Minh <minhzzz3614534@gmail.com>
2019-10-15 16:01:37 +0700

    query for sql

---
+++ SQLQuery3.sql
@@ -0,0 +1,43 @@
+USE Book
+GO
+/*Tao ban khach hang(MaKH la so CMND)*/
+CREATE TABLE KhachHang
+(
+	MaKH nvarchar(20),
+	HoTen nvarchar(50),
+	SDT nvarchar(20),
+	PRIMARY KEY (MaKH)	
+)
+/*Tao ban don hang(Ma don hang la so nguyen tu dong tang)*/
+GO
+CREATE TABLE DonHang
+(
+	MaDH INT IDENTITY(1,1),
+	MaKH nvarchar(20),
+	NgayTra Date,
+	ThanhTien INT,
+	TinhTrang BIT,
+	PRIMARY KEY (MaDH),
+	FOREIGN KEY (MaKH) references KhachHang
+)
+/*Tao ban sach(Ma sach la so nguyen tu dong tang)*/
+GO
+CREATE TABLE Sach
+(
+	MaS INT IDENTITY(1,1),
+	TenSach nvarchar(20),
+	GiaSach INT,
+	SLS INT,
+	PRIMARY KEY(MaS)
+)
+/*Tao ban DH-Sach the hien moi quen he nhieu nhieu*/
+GO
+CREATE TABLE DH_SACH
+(
+	MaDH INT,
+	MaS INT,
+	SLM INT,
+	PRIMARY KEY (MaDH, MaS),
+	FOREIGN KEY (MaDH) references DonHang,
+	FOREIGN KEY (MaS) references Sach
+)
