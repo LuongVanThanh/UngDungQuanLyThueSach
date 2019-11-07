eb102a6036426c80b7127d80e12d9f6b56322967
hoaithanh <1751010136thanh@ou.edu.vn>
2019-11-08 00:02:52 +0700

    update chuc nang thuesach

--- QuanLyThueSach/src/main/ThueSachJFrame.java
+++ QuanLyThueSach/src/main/ThueSachJFrame.java
@@ -1,12 +1,19 @@
-
 package main;
 
 import Classes.DonHang;
+import Classes.KhachHang;
+import Classes.Sach;
 import java.awt.Toolkit;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.table.DefaultTableModel;
+import org.omg.IOP.CodecPackage.FormatMismatch;
 
 public class ThueSachJFrame extends javax.swing.JFrame {
 
@@ -15,6 +22,7 @@ public class ThueSachJFrame extends javax.swing.JFrame {
         setTitle("Thuê Sách");
         setLocation(300, 150);
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/image/IconApp.png")));
+        init();
     }
 
     /**
@@ -30,7 +38,7 @@ public class ThueSachJFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jbtVTC = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jtbThueSach = new javax.swing.JTable();
+        jtbSach = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jtfMaKH = new javax.swing.JTextField();
@@ -41,6 +49,8 @@ public class ThueSachJFrame extends javax.swing.JFrame {
         jlbNgayTra = new javax.swing.JLabel();
         jlbTongTien = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jtfSoLuong = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -80,13 +90,13 @@ public class ThueSachJFrame extends javax.swing.JFrame {
                     .addComponent(jbtVTC, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)))
         );
 
-        jtbThueSach = new javax.swing.JTable(){
+        jtbSach = new javax.swing.JTable(){
             public boolean isCellEditable(int rowIndex, int colIndex){
                 return false;
             }
         };
-        jtbThueSach.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
-        jtbThueSach.setModel(new javax.swing.table.DefaultTableModel(
+        jtbSach.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        jtbSach.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -109,18 +119,18 @@ public class ThueSachJFrame extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jtbThueSach.getTableHeader().setResizingAllowed(false);
-        jtbThueSach.getTableHeader().setReorderingAllowed(false);
-        jScrollPane1.setViewportView(jtbThueSach);
-        if (jtbThueSach.getColumnModel().getColumnCount() > 0) {
-            jtbThueSach.getColumnModel().getColumn(0).setResizable(false);
-            jtbThueSach.getColumnModel().getColumn(0).setPreferredWidth(50);
-            jtbThueSach.getColumnModel().getColumn(1).setResizable(false);
-            jtbThueSach.getColumnModel().getColumn(1).setPreferredWidth(150);
-            jtbThueSach.getColumnModel().getColumn(2).setResizable(false);
-            jtbThueSach.getColumnModel().getColumn(2).setPreferredWidth(30);
-            jtbThueSach.getColumnModel().getColumn(3).setResizable(false);
-            jtbThueSach.getColumnModel().getColumn(3).setPreferredWidth(50);
+        jtbSach.getTableHeader().setResizingAllowed(false);
+        jtbSach.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(jtbSach);
+        if (jtbSach.getColumnModel().getColumnCount() > 0) {
+            jtbSach.getColumnModel().getColumn(0).setResizable(false);
+            jtbSach.getColumnModel().getColumn(0).setPreferredWidth(50);
+            jtbSach.getColumnModel().getColumn(1).setResizable(false);
+            jtbSach.getColumnModel().getColumn(1).setPreferredWidth(150);
+            jtbSach.getColumnModel().getColumn(2).setResizable(false);
+            jtbSach.getColumnModel().getColumn(2).setPreferredWidth(30);
+            jtbSach.getColumnModel().getColumn(3).setResizable(false);
+            jtbSach.getColumnModel().getColumn(3).setPreferredWidth(50);
         }
 
         jLabel2.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
@@ -135,6 +145,11 @@ public class ThueSachJFrame extends javax.swing.JFrame {
 
         jbtXuatPhieu.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         jbtXuatPhieu.setText("Xuất phiếu");
+        jbtXuatPhieu.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtXuatPhieuMouseClicked(evt);
+            }
+        });
 
         jbtThem.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         jbtThem.setText("Thêm");
@@ -159,6 +174,11 @@ public class ThueSachJFrame extends javax.swing.JFrame {
         jLabel5.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         jLabel5.setText("Tổng tiền:");
 
+        jLabel6.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        jLabel6.setText("Số lượng:");
+
+        jtfSoLuong.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -168,12 +188,16 @@ public class ThueSachJFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jtfMaS, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
-                    .addComponent(jtfMaKH, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
-                    .addComponent(jbtThem, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jtfMaS, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
+                    .addComponent(jtfMaKH, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jbtThem, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtfSoLuong))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jbtXuatPhieu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -197,18 +221,22 @@ public class ThueSachJFrame extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
                             .addComponent(jtfMaKH, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(30, 30, 30)
+                        .addGap(20, 20, 20)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
                             .addComponent(jtfMaS, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(20, 20, 20)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(jtfSoLuong, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(14, 14, 14)
                         .addComponent(jbtThem))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel4)
-                        .addComponent(jlbNgayTra, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jlbNgayTra, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jlbTongTien, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel5))
                 .addGap(29, 29, 29)
@@ -218,20 +246,99 @@ public class ThueSachJFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+    
+    DonHang dh = new DonHang();
+    float t = 0;
+    String[] arr = {"Mã sách", "Tên sách", "Số lượng thuê", "Giá"};
+    DefaultTableModel model;
+    
     private void jbtVTCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtVTCActionPerformed
         this.setVisible(false);
         new MainJFrame().setVisible(true);
     }//GEN-LAST:event_jbtVTCActionPerformed
-
     
-    private void jbtThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtThemActionPerformed
+    private void init(){
+        jtbSach.removeAll();
+        model = new DefaultTableModel(arr, 0);
+        jtbSach.setModel(model);
+        jtfMaKH.setEnabled(true);
+        jtfMaKH.setText("");
+        jtfMaS.setText("");
+        jtfSoLuong.setText("");
+        jlbNgayTra.setText("");
+        jlbTongTien.setText("");
+    }
+    
 
+    private void jbtThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtThemActionPerformed
     }//GEN-LAST:event_jbtThemActionPerformed
 
+    //thêm từng cuốn sách vào bảng và add vào đối tượng dh(đơn hàng)
     private void jbtThemMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtThemMouseClicked
-        jtfMaKH.setEnabled(false);
+        try {
+            
+            String maKH = jtfMaKH.getText();
+            String maS = jtfMaS.getText();
+            int soLM = Integer.parseInt(jtfSoLuong.getText());
+            KhachHang kh = new KhachHang();
+            Sach s = new Sach();
+            if ("".equals(maKH)) {
+                JOptionPane.showMessageDialog(null, "Bạn chưa nhập mã khách hàng!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                jtfMaKH.requestFocus();
+            } else if ("".equals(maS)) {
+                JOptionPane.showMessageDialog(null, "Bạn chưa nhập mã sách!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                jtfMaS.requestFocus();
+            } else if ("".equals(jtfSoLuong.getText())) {
+                JOptionPane.showMessageDialog(null, "Bạn chưa nhập số lượng thuê!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                jtfSoLuong.requestFocus();
+            } else if (kh.TimKH(maKH) == null) {
+                JOptionPane.showMessageDialog(null, "Dữ liệu mã khách hàng không đúng!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                jtfMaKH.requestFocus();
+            } else if(s.TimSach(maS) == null){
+                JOptionPane.showMessageDialog(null, "Dữ liệu mã sách không đúng!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                jtfMaS.requestFocus();
+            } else{
+                jtfMaKH.setEnabled(false);
+                if(soLM <= s.getSLS()){
+                    dh.ThemSach(maS, String.valueOf(soLM));
+                    Vector v = new Vector();
+                    v.add(maS);
+                    v.add(s.getTenSach());
+                    v.add(soLM);
+                    v.add(s.getGia()*soLM);
+                    model.addRow(v);
+                    jtbSach.setModel(model);
+                    jtfMaS.setText("");
+                    jtfSoLuong.setText("");
+                    jtfMaS.requestFocus();
+                    if("".equals(jlbNgayTra.getText()))
+                        //xuất ngày trả và format dd-MM-yyyy
+                        jlbNgayTra.setText(LocalDate.now().plusMonths(1).format(DateTimeFormatter.ofPattern("dd-MM-yyyy")).toString());
+                    t += s.getGia() * soLM;
+                    //xuất tổng tiền của đơn hàng
+                    jlbTongTien.setText(String.format("%,.2f VNĐ", t));
+                } else {
+                    JOptionPane.showMessageDialog(null, "Số lượng sách không đủ!!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    jtfSoLuong.requestFocus();
+                }
+                    
+            }
+
+        }catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Dữ liệu số lượng hơp lệ!!!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        
     }//GEN-LAST:event_jbtThemMouseClicked
+
+    private void jbtXuatPhieuMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtXuatPhieuMouseClicked
+        //thêm dữ liệu vào sql
+        try {
+            dh.ThemDonHang(jtfMaKH.getText());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ThueSachJFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        init();
+    }//GEN-LAST:event_jbtXuatPhieuMouseClicked
 
     /**
      * @param args the command line arguments
@@ -272,7 +379,7 @@ public class ThueSachJFrame extends javax.swing.JFrame {
                 } catch (IllegalAccessException ex) {
                     Logger.getLogger(ThueSachJFrame.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (UnsupportedLookAndFeelException ex) {
-                    Logger.getLogger(ThueSachJFrame.class.getName()).log(Level.SEVERE, null, ex);  
+                    Logger.getLogger(ThueSachJFrame.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 new ThueSachJFrame().setVisible(true);
             }
@@ -285,6 +392,7 @@ public class ThueSachJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton jbtThem;
@@ -292,8 +400,9 @@ public class ThueSachJFrame extends javax.swing.JFrame {
     private javax.swing.JButton jbtXuatPhieu;
     private javax.swing.JLabel jlbNgayTra;
     private javax.swing.JLabel jlbTongTien;
-    private javax.swing.JTable jtbThueSach;
+    private javax.swing.JTable jtbSach;
     private javax.swing.JTextField jtfMaKH;
     private javax.swing.JTextField jtfMaS;
+    private javax.swing.JTextField jtfSoLuong;
     // End of variables declaration//GEN-END:variables
 }
